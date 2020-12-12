fa641b0a46564ae3521a70bc10d86131d1c3690d
Adrian Krasowski <ADRIAN433@bailliegifford.com>
2020-12-12 20:10:26 +0100

    Language selection and routing

--- Callan/src/app/navigation/lang-selection-tab/lang-selection-tab.component.ts
+++ Callan/src/app/navigation/lang-selection-tab/lang-selection-tab.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageService } from '../utility/services/language-provider.service';
 import { LanguageModel } from '../utility/models/language-model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lang-selection-tab',
@@ -10,11 +11,22 @@ import { LanguageModel } from '../utility/models/language-model';
 export class LangSelectionTabComponent implements OnInit {
   public selectableLanguages: LanguageModel[] = [];
 
-  constructor(private languageService: LanguageService) {}
+  constructor(
+    private languageService: LanguageService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe(value => {
+      let lang = value['lang'];
+      console.log(lang);
+    });
     this.languageService.getLanguageList().subscribe(languages => {
       this.selectableLanguages = languages;
+      this.route.params.subscribe(value => {
+        let lang = value['lang'];
+        console.log(lang);
+      });
     });
   }
 }
